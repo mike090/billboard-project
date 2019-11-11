ff254cf703351a6990f2b54f655fdfbc4f57f24c
mike090 <mike09@mail.ru>
2019-11-11 08:33:29 +0300

    time zone, уникальный индекс по полю name рубрики, verbose_name приложения bboard

--- bboard/models.py
+++ bboard/models.py
@@ -15,7 +15,7 @@ class BillBoard(models.Model):
 		ordering = ['-published']
 
 class Rubric(models.Model):
-	name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+	name = models.CharField(max_length=20, unique=True, verbose_name='Название')
 
 	def __str__(self):
 		return self.name
